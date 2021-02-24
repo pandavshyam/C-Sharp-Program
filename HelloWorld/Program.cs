e2b185d199968dc8661597c05b96fc09c0fac28c
Shyam Pandav <pandavshyam@gmail.com>
2021-02-25 03:08:46 +0530

    Added Primitive Datatypes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Printing Hello World
             Console.WriteLine("Hello World!");
+
+            // Printing Primitive datatypes
+            byte number = 250;
+            float floatNum = 10.1f;
+            char charType = 'A';
+            string name = "Shyam";
+            bool isLoggedIn = false;
+            var varType = 200;
+            Console.WriteLine(varType);
         }
     }
 }
