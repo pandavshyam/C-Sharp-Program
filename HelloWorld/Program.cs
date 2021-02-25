ad6c5b453ae265daa434617cebd9ec17c228a0c9
Shyam Pandav <pandavshyam@gmail.com>
2021-02-26 01:12:19 +0530

    Added Type Conversion, Exception Handling Basic Programs

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -17,6 +17,32 @@ namespace HelloWorld
             bool isLoggedIn = false;
             var varType = 200;
             Console.WriteLine(varType);
+
+            // byte minvalue
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+
+            // float minvalue
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+            // Implicit type conversion
+            byte a = 2;
+            int b = a; // a is converted into int
+
+            // Explicit type conversion
+            int c = 10;
+            byte d = (byte)c; //  Data loss can be there
+
+            // Exception Handling
+            try
+            {
+                string str = "1234";
+                byte strToByte = Convert.ToByte(str);
+                Console.WriteLine(strToByte);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception Occured");
+            }
         }
     }
 }
