7b316a66032ddc44f9572f8a4243c6ec5df30273
Shyam Pandav <pandavshyam@gmail.com>
2021-03-02 03:13:24 +0530

    Added Exception Code (User Defined)

---
+++ HelloWorld/ExceptionHandling.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class ExceptionHandling
+    {
+        // User defind Exception
+        class OddNumberException : Exception
+        {
+            public OddNumberException(string Message) : base(Message)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Simple Exception Code
+            try
+            {
+                int i = 1;
+                int j = 10;
+                Console.WriteLine(j/i);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            // User defined
+            try
+            {
+                OddNumber();
+            }
+            catch (OddNumberException)
+            {
+                Console.WriteLine("User defined exception");
+            }
+            
+        }
+
+        static void MultipleExceptionHandling()
+        {
+            try
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by Zero Exception");
+            }
+        }
+
+        static void OddNumber()
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num %2 != 0)
+            {
+                throw new OddNumberException("Number is odd");
+            }
+            Console.WriteLine("Number is Even");
+        }
+    }
+}
---
+++ HelloWorld/outKeyword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class outKeyword
+    {
+        static void Main(string[] args)
+        {
+            Employee(10, out int sal, out string nameEmp);
+            Console.WriteLine(sal);
+            Console.WriteLine(nameEmp);
+        }
+        // Out keyword is used when we want to return multiple values from method (with different datatypes
+        static void Employee(int id, out int salary, out string name)
+        {
+            salary = 1000;
+            name = "Shyam";
+        }
+    }
+}
