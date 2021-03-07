3793852970ad19c5c964bfa6e0950435f2af3c1a
Shyam Pandav <pandavshyam@gmail.com>
2021-03-07 23:46:12 +0530

    Added Practice Problems

--- C-Sharp-Introduction/Problem2.cs
+++ C-Sharp-Introduction/Problem2.cs
@@ -10,7 +10,25 @@ namespace C_Sharp_Introduction
     {
         static void Main(string[] args)
         {
+            string mainString = "empty", subString = "d";
+            Console.WriteLine(HowManyOccurrences(mainString, subString));
+        }
 
+        private static int HowManyOccurrences(string mainString, string subString)
+        {
+            int count = 0;
+            for (int i = 0; i <= mainString.Length - subString.Length; i++)
+            {
+                int j;
+                for (j = 0; j < subString.Length; j++)
+                {
+                    if (mainString[i + j] != subString[j])
+                        break;
+                }
+                if (j == subString.Length)
+                    count++;
+            }
+            return count;
         }
     }
 }
---
+++ C-Sharp-Introduction/Problem3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Introduction
+{
+    class Problem3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IfNumberContains5(87301));
+        }
+
+        private static bool IfNumberContains5(int num)
+        {
+            while (num != 0)
+            {
+                int remainder = num % 10;
+                if (remainder == 5)
+                    return true;
+                num = num / 10;
+            }
+            return false;
+        }
+    }
+}
